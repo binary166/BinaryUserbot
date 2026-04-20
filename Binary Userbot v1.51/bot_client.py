a5a8f91d27665a1444efcb28d41f908ad8afc4b6
binary166 <info7281o@gmail.com>
2026-04-20 19:27:16 +0300

    Delete Binary Userbot v1.51 directory

--- Binary Userbot v1.51/bot_client.py
+++
@@ -1,8 +0,0 @@
-"""
-Единственный экземпляр TelegramClient.
-Все модули импортируют client отсюда.
-"""
-from telethon import TelegramClient
-from config import SESSION_NAME, API_ID, API_HASH
-
-client = TelegramClient(SESSION_NAME, API_ID, API_HASH)
